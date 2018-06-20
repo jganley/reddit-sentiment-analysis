dcc93978c7f42bc594bb8be1494f9092b5e6c8f3
Joey Ganley <jganley@g.ucla.edu>
2018-06-20 01:58:37 -0700

    trained the cv model

--- analysis.py
+++ analysis.py
@@ -17,7 +17,6 @@ def write_parquet(sqlContext):
     comments.write.parquet("superbowl_comments.parquet")
 
 def create_dataframe(sqlContext, comments, labels):
-
     comments.registerTempTable("commentsTable")
     labels.registerTempTable("labelsTable")
 
@@ -30,6 +29,22 @@ def create_dataframe(sqlContext, comments, labels):
     labeledData = labeledData.withColumn("udf_results", parse_udf(col("comments_body")))
 
     return labeledData
+
+def train_cv_model(modelDataframe):
+    cv = CountVectorizer(inputCol="udf_results", outputCol="features", binary=True, minDF=5.0)
+    model = cv.fit(modelDataframe)
+    model.write().overwrite().save("cvModel")
+
+def transform_model(modelDataframe, labels):
+    labels.registerTempTable("labelsTable")
+    # Load the CV model
+    model = CountVectorizerModel.load("cvModel")
+    # Transform the data frame
+    transformedDf = model.transform(modelDataframe)
+    transformedDf.registerTempTable("transformedDfTable")
+    transformedDf = sqlContext.sql("SELECT transformedDfTable.comments_id AS id, transformedDfTable.comments_body AS body, transformedDfTable.comments_author AS author, transformedDfTable.comments_created_utc AS created_utc, transformedDfTable.comments_subreddit_id AS subreddit_id, transformedDfTable.comments_link_id AS link_id, transformedDfTable.comments_parent_id AS parent_id, transformedDfTable.comments_score AS score, transformedDfTable.comments_controversiality AS controversiality, transformedDfTable.comments_gilded AS gilded, transformedDfTable.udf_results AS udf_results, transformedDfTable.features AS features, IF(labelsTable.label=1, 1, 0) AS pos_label, IF(labelsTable.label=-1, 1, 0) AS neg_label FROM transformedDfTable INNER JOIN labelsTable ON transformedDfTable.comments_id = labelsTable.label_id")
+
+    return transformedDf
 
 def main(sqlContext):
 
@@ -48,7 +63,16 @@ def main(sqlContext):
     # Create Dataframe to Train the Model
     modelDataframe = create_dataframe(sqlContext, comments, labels)
 
+    # Fit the CountVectorizer model
+    if(not os.path.exists("cvModel")):
+        train_cv_model(modelDataframe)
+
+    # Use model to transform the data
+    modelDataframe = transform_model(modelDataframe, labels)
+    modelDataframe.registerTempTable("modelDataframeTable")
+
     modelDataframe.show()
+
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("SuperBowl Sentiment Analysis")
