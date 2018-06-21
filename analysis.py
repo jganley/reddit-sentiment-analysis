d704b9c35667bc5a543a66e1c9fd1e602eae97e3
Joey Ganley <jganley@g.ucla.edu>
2018-06-20 20:45:12 -0700

    trained positive a negative models

--- analysis.py
+++ analysis.py
@@ -33,18 +33,64 @@ def create_dataframe(sqlContext, comments, labels):
 def train_cv_model(modelDataframe):
     cv = CountVectorizer(inputCol="udf_results", outputCol="features", binary=True, minDF=5.0)
     model = cv.fit(modelDataframe)
-    model.write().overwrite().save("cvModel")
+    model.write().overwrite().save("models/cvModel")
 
-def transform_model(modelDataframe, labels):
+def transform_model(sqlContext, modelDataframe, labels):
     labels.registerTempTable("labelsTable")
     # Load the CV model
-    model = CountVectorizerModel.load("cvModel")
+    model = CountVectorizerModel.load("models/cvModel")
     # Transform the data frame
     transformedDf = model.transform(modelDataframe)
     transformedDf.registerTempTable("transformedDfTable")
     transformedDf = sqlContext.sql("SELECT transformedDfTable.comments_id AS id, transformedDfTable.comments_body AS body, transformedDfTable.comments_author AS author, transformedDfTable.comments_created_utc AS created_utc, transformedDfTable.comments_subreddit_id AS subreddit_id, transformedDfTable.comments_link_id AS link_id, transformedDfTable.comments_parent_id AS parent_id, transformedDfTable.comments_score AS score, transformedDfTable.comments_controversiality AS controversiality, transformedDfTable.comments_gilded AS gilded, transformedDfTable.udf_results AS udf_results, transformedDfTable.features AS features, IF(labelsTable.label=1, 1, 0) AS pos_label, IF(labelsTable.label=-1, 1, 0) AS neg_label FROM transformedDfTable INNER JOIN labelsTable ON transformedDfTable.comments_id = labelsTable.label_id")
 
     return transformedDf
+
+def create_models(sqlContext, modelDataframe):
+    modelDataframe.registerTempTable("modelDataframeTable")
+
+    # Create dataframes to use on the positive and negative models
+    pos = sqlContext.sql("SELECT pos_label AS label, features FROM modelDataframeTable")
+    neg = sqlContext.sql("SELECT neg_label AS label, features FROM modelDataframeTable")
+
+    # Initialize two logistic regression models.
+    # Replace labelCol with the column containing the label, and featuresCol with the column containing the features.
+    poslr = LogisticRegression(labelCol="label", featuresCol="features", maxIter=10).setThreshold(0.2)
+    neglr = LogisticRegression(labelCol="label", featuresCol="features", maxIter=10).setThreshold(0.25)
+    # This is a binary classifier so we need an evaluator that knows how to deal with binary classifiers.
+    posEvaluator = BinaryClassificationEvaluator()
+    negEvaluator = BinaryClassificationEvaluator()
+    # There are a few parameters associated with logistic regression. We do not know what they are a priori.
+    # We do a grid search to find the best parameters. We can replace [1.0] with a list of values to try.
+    # We will assume the parameter is 1.0. Grid search takes forever.
+    posParamGrid = ParamGridBuilder().addGrid(poslr.regParam, [1.0]).build()
+    negParamGrid = ParamGridBuilder().addGrid(neglr.regParam, [1.0]).build()
+    # We initialize a 5 fold cross-validation pipeline.
+    posCrossval = CrossValidator(
+        estimator=poslr,
+        evaluator=posEvaluator,
+        estimatorParamMaps=posParamGrid,
+        numFolds=2)
+    negCrossval = CrossValidator(
+        estimator=neglr,
+        evaluator=negEvaluator,
+        estimatorParamMaps=negParamGrid,
+        numFolds=2)
+    # Although crossvalidation creates its own train/test sets for
+    # tuning, we still need a labeled test set, because it is not
+    # accessible from the crossvalidator (argh!)
+    # Split the data 50/50
+    posTrain, posTest = pos.randomSplit([0.5, 0.5])
+    negTrain, negTest = neg.randomSplit([0.5, 0.5])
+    # Train the models
+    print("Training positive classifier...")
+    posModel = posCrossval.fit(posTrain)
+    print("Training negative classifier...")
+    negModel = negCrossval.fit(negTrain)
+
+    # Once we train the models, we don't want to do it again. We can save the models and load them again later.
+    posModel.write().overwrite().save("models/posModel")
+    negModel.write().overwrite().save("models/negModel")
 
 def main(sqlContext):
 
@@ -64,14 +110,19 @@ def main(sqlContext):
     modelDataframe = create_dataframe(sqlContext, comments, labels)
 
     # Fit the CountVectorizer model
-    if(not os.path.exists("cvModel")):
+    if(not os.path.exists("models/cvModel")):
         train_cv_model(modelDataframe)
 
     # Use model to transform the data
-    modelDataframe = transform_model(modelDataframe, labels)
-    modelDataframe.registerTempTable("modelDataframeTable")
+    modelDataframe = transform_model(sqlContext, modelDataframe, labels)
 
-    modelDataframe.show()
+    if(not os.path.exists("models/negModel") or not os.path.exists("models/posModel")):
+        create_models(sqlContext, modelDataframe)
+
+    # Load the positive and negative models back in
+    posModel = CrossValidatorModel.load("models/posModel")
+    negModel = CrossValidatorModel.load("models/negModel")
+
 
 
 if __name__ == "__main__":
